4ad63b278c75faa695e1a1393eb4b9c4daf854d8
mikecoram <mikecoram96@gmail.com>
2015-10-22 19:19:55 +0100

    Live web version

--- js/data.js
+++ js/data.js
@@ -1,9 +1,10 @@
 //Data structures
 //Notes, chords & keys
-var note = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 
-				'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];
-var keyTypes = ['major', 'minor'];
-var chordTypes = ['major', 'minor', 'diminished', 'augmented'];
+var note = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", 
+				"E", "F", "F#/Gb", "G", "G#/Ab"];
+
+var keyTypes = ["major", "minor"];
+var chordTypes = ["major", "minor", "augmented", "diminished"];
 
 //Relative chord and note details
 function relChord(inputChordType, inputKeyName, inputDegree) {
@@ -36,43 +37,37 @@ function storeChordType(name,amount,color) {
 }
 
 //major chord
-var majcolor = '#FFFF9D'
-storeChordType('major',3,majcolor);
-storeNote('majornote1','major',0,'I');
-storeNote('majornote2','major',4,'III');
-storeNote('majornote3','major',7,'V');
+var majcolor = "#FFFF9D"
+storeChordType("major",3,majcolor);
+storeNote("majornote1","major",0,"I");
+storeNote("majornote2","major",4,"III");
+storeNote("majornote3","major",7,"V");
 //minor chord
-var mincolor = '#BEEB9F'
-storeChordType('minor',3,mincolor);
-storeNote('minornote1','minor',0,'I');
-storeNote('minornote2','minor',3,'IIIb');
-storeNote('minornote3','minor',7,'V');
+var mincolor = "#BEEB9F"
+storeChordType("minor",3,mincolor);
+storeNote("minornote1","minor",0,"I");
+storeNote("minornote2","minor",3,"IIIb");
+storeNote("minornote3","minor",7,"V");
 //diminished chord
-var dimcolor = '#FF6138'
-storeChordType('diminished',3,dimcolor);
-storeNote('diminishednote1','diminished',0,'I');
-storeNote('diminishednote2','diminished',3,'IIIb');
-storeNote('diminishednote3','diminished',6,'Vb');
-//augmented chord
-var augcolor = 'red';
-storeChordType('augmented',3,augcolor);
-storeNote('augmentednote1','augmented',0,'I');
-storeNote('augmentednote2','augmented',4,'III#');
-storeNote('augmentednote3','augmented',4,'V#');
+var dimcolor = "#FF6138"
+storeChordType("diminished",3,dimcolor);
+storeNote("diminishednote1","diminished",0,"I");
+storeNote("diminishednote2","diminished",3,"IIIb");
+storeNote("diminishednote3","diminished",6,"Vb");
 
 //major key
-storeChord('majorchord1','major','major',1);
-storeChord('majorchord2','minor','major',1);
-storeChord('majorchord3','minor','major',3);
-storeChord('majorchord4','major','major',4);
-storeChord('majorchord5','major','major',5);
-storeChord('majorchord6','minor','major',6);
-storeChord('majorchord7','diminished','major',7);
+storeChord("majorchord1","major","major",1);
+storeChord("majorchord2","minor","major",1);
+storeChord("majorchord3","minor","major",3);
+storeChord("majorchord4","major","major",4);
+storeChord("majorchord5","major","major",5);
+storeChord("majorchord6","minor","major",6);
+storeChord("majorchord7","diminished","major",7);
 //minor key
-storeChord('minorchord1','minor','minor',1);
-storeChord('minorchord2','diminished','minor',2);
-storeChord('minorchord3','major','minor',3);
-storeChord('minorchord4','minor','minor',4);
-storeChord('minorchord5','minor','minor',5);
-storeChord('minorchord6','major','minor',6);
-storeChord('minorchord7','major','minor',7);
+storeChord("minorchord1","minor","minor",1);
+storeChord("minorchord2","diminished","minor",2);
+storeChord("minorchord3","major","minor",3);
+storeChord("minorchord4","minor","minor",4);
+storeChord("minorchord5","minor","minor",5);
+storeChord("minorchord6","major","minor",6);
+storeChord("minorchord7","major","minor",7);
